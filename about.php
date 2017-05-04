e3d5f8240f028ee742b0d85425ecdacf6191791f
derijumara <derijumara97@gmail.com>
2017-05-04 15:50:50 +0700

    Update about.php

--- about.php
+++ about.php
@@ -4,7 +4,8 @@
              <div align="center">   
                 <h2>SILAPAR</h2>
                 <p>Silaper adalah sebuah aplikasi berbasis web dan Android yang digunakan untuk melayani keluhan masyarakat yang ada di wilayah Kabupaten Bandung.</p>
-                <p>Alur kerja dari aplikasi ini adalah masyarakat memasukkan keluhan yang dialami untuk sebuah Dinas di pemkab Bandung yang terkait. Kemudian keluhan akan diterima oleh admin aplikasi dan diikirim kepada dinas terkait dan di terima oleh kepala bagian humas Dinias untuk selanjutnya diteruskan kepada Dinas tersebut. Keluhan tersebut di proses kemudian akan di sampaikan jawabannya memalui akun khusus untuk kepala bagian humas dinas tersebut.</p>
+                
+                 <p>Alur kerja dari aplikasi ini adalah masyarakat memasukkan keluhan yang dialami untuk sebuah Dinas di pemkab Bandung yang terkait. Kemudian keluhan akan diterima oleh admin aplikasi dan diikirim kepada dinas terkait dan di terima oleh kepala bagian humas Dinias untuk selanjutnya diteruskan kepada Dinas tersebut. Keluhan tersebut di proses kemudian akan di sampaikan jawabannya memalui akun khusus untuk kepala bagian humas dinas tersebut.</p>
             </div>
         </div>
     </div>
